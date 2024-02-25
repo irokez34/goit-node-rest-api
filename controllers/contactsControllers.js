5ce1aec241ec2deb00bfdc8ed4dab8a5b9fed019
irokez34 <jhmdaurel@gmail.com>
2024-02-25 20:22:04 +0200

    new-code

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,4 +1,6 @@
+import HttpError from "../helpers/HttpError.js";
 import Contact from "../model/contactsSchema.js";
+import { createContactSchema } from "../schemas/contactsSchemas.js";
 
 export const getAllContacts = async (req, res, next) => {
   try {
@@ -9,15 +11,38 @@ export const getAllContacts = async (req, res, next) => {
   }
 };
 
-export const getOneContact = (req, res) => {
-  res.send("getOneContact");
+export const getOneContact = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const contact = await Contact.findById(id);
+
+    res.send(contact);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 };
 
-export const deleteContact = (req, res) => {
-  res.send("deleteContact");
+export const deleteContact = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const contact = await Contact.findByIdAndDelete(id);
+
+    res.send(contact);
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const createContact = (req, res) => {
+export const createContact = async (req, res, next) => {
+  const body = req.body;
+  const newContact = (validateBody(createContactSchema), body);
+  console.log(newContact);
+  try {
+    const contact = await Contact.create();
+  } catch (error) {}
   res.send("createContact");
 };
 
