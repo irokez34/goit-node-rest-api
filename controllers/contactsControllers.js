0f36206ae5494a45839fdce3aa3c0803e2072a74
irokez34 <jhmdaurel@gmail.com>
2024-02-25 18:29:40 +0200

    new-files

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,26 @@
-import contactsService from "../services/contactsServices.js";
+import Contact from "../model/contactsSchema.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const contacts = await Contact.find();
+    res.send(contacts);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = (req, res) => {
+  res.send("getOneContact");
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = (req, res) => {
+  res.send("deleteContact");
+};
 
-export const createContact = (req, res) => {};
+export const createContact = (req, res) => {
+  res.send("createContact");
+};
 
-export const updateContact = (req, res) => {};
+export const updateContact = (req, res) => {
+  res.send("updateContact");
+};
